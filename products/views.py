8278b6d6f1e3baff86308786f672adfcdff4e930
Rafaelkrc <rafaelkrc@hotmail.com>
2024-10-16 20:47:07 +0100

    created product list template
    close #11

--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
+from products.models import Product
 
 
 class ProductListView(ListView):
-    ...
+    model = Product
+    template_name = 'products/list.html'
+    context_object_name = 'products'
+    paginate_by = 10
 
 
 class ProductDetailView(View):
