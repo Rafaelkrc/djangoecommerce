a7b8474612fcca024b1a840b40b826233ed54d8e
Rafaelkrc <rafaelkrc@hotmail.com>
2024-10-21 12:08:49 +0100

    create add cart and session
    close #13

--- products/urls.py
+++ products/urls.py
@@ -5,9 +5,9 @@ app_name = 'product'
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='list'),
-    path('<slug>/', views.ProductDetailView.as_view(), name='detail'),
     path('addtocart/', views.AddToCartView.as_view(), name='addtocart'),
     path('removetocart/', views.RemoveToCartView.as_view(), name='removetocart'),
     path('cart/', views.CartView.as_view(), name='cart'),
     path('finalise/', views.FinaliseView.as_view(), name='finalise'),
+    path('<slug>/', views.ProductDetailView.as_view(), name='detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, resolve_url, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views import View
-from products.models import Product
+from products.models import Product, Variation
 from django.http import HttpResponse
+from django.contrib import messages
+
+from pprint import pprint
 
 
 class ProductListView(ListView):
@@ -21,7 +24,79 @@ class ProductDetailView(DetailView):
 
 class AddToCartView(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('Adicionar carrinho')
+        http_referer = self.request.META.get(
+            'HTTP_REFERER', resolve_url('product:list'))
+        variation_id = self.request.GET.get('vid')
+
+        if not variation_id:
+            messages.error(self.request, 'Produto não existe!')
+            return redirect(http_referer)
+
+        variation = get_object_or_404(Variation, id=variation_id)
+        variation_stock = variation.stock
+        product = variation.product
+
+        product_id = product.id  # type: ignore
+        product_name = product.name
+        variation_name = variation.name or ''
+        unit_price = variation.price
+        unit_promotional_price = variation.promotional_price
+        slug = product.slug
+        image = product.image
+
+        if image:
+            image = image.name
+        else:
+            image = ''
+
+        if variation.stock < 1:
+            messages.error(
+                self.request, 'Estoque insuficiente!'
+            )
+            return redirect(http_referer)
+
+        if not self.request.session.get('cart'):
+            self.request.session['cart'] = {}
+            self.request.session.save()
+
+        cart = self.request.session['cart']
+
+        if variation_id in cart:
+            cart_quantity = cart[variation_id]['quantity']
+            cart_quantity += 1
+
+            if variation_stock < cart_quantity:
+                messages.error(
+                    self.request,
+                    f'Estoque insuficiente para {
+                        cart_quantity}x no produto "{product_name}".'
+                    f'Adicionamos {variation_stock}x no seu carrinho.'
+                )
+                cart_quantity = variation_stock
+
+            cart[variation_id]['quantity'] = cart_quantity
+            cart[variation_id]['quantitative_price'] = unit_price * cart_quantity
+            cart[variation_id]['quantitative_promotional_price'] = unit_promotional_price * cart_quantity
+        else:
+            cart[variation_id] = {
+                'product_id': product_id,
+                'product_name': product_name,
+                'variation_name': variation_name,
+                'variation_id': variation_id,
+                'unit_price': unit_price,
+                'unit_promotional_price': unit_promotional_price,
+                'quantity': 1,
+                'slug': slug,
+                'image': image,
+            }
+        self.request.session.save()
+        messages.success(
+            self.request,
+            f'Produto {product.name} {
+                variation_name} adicionado ao seu carrinho,'
+            f' total de {cart[variation_id]['quantity']} unidades!'
+        )
+        return redirect(http_referer)
 
 
 class RemoveToCartView(View):
@@ -29,7 +104,8 @@ class RemoveToCartView(View):
 
 
 class CartView(View):
-    ...
+    def get(self, *args, **kwargs):
+        return render(self.request, 'products/cart.html')
 
 
 class FinaliseView(View):
